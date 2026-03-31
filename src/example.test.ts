6e3b7a077d418a32b59433ede4c7e41923e082bc
jalexw <jalexwhitman@gmail.com>
2026-03-31 10:01:54 -0300

    0.0.2 - add another test, bump version

--- src/example.test.ts
+++ src/example.test.ts
@@ -5,7 +5,7 @@ import {
 } from "./send-email-request-body-schema";
 
 describe("Example Message", () => {
-  test("can parse successfully", () => {
+  test("can parse a simple message successfully", () => {
     const parsed = sendEmailRequestBodySchema.safeParse({
       to: "support@schemavaults.com",
       subject: "Hello World!",
@@ -13,6 +13,19 @@ describe("Example Message", () => {
         html: "<html><body><h1>Hello World!</h1></body></html>",
         text: "Hello World!",
       },
+    } satisfies SendEmailRequestBody);
+    expect(parsed.success).toBeTrue();
+  });
+
+  test("can parse a message with a 'from' sender successfully", () => {
+    const parsed = sendEmailRequestBodySchema.safeParse({
+      to: "support@schemavaults.com",
+      subject: "Hello World!",
+      message: {
+        html: "<html><body><h1>Hello World!</h1></body></html>",
+        text: "Hello World!",
+      },
+      from: "support@schemavaults.com",
     } satisfies SendEmailRequestBody);
     expect(parsed.success).toBeTrue();
   });
